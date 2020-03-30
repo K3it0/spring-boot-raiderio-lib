6ec535f6f4c534c0624ae31db3f15bac84e0d8ac
K3it0 <dev@frederickeite.de>
2020-03-30 16:15:05 +0200

    Init maven project

---
+++ src/test/java/de/keitocode/springbootraideriolib/SpringBootRaiderioLibApplicationTests.java
@@ -0,0 +1,13 @@
+package de.keitocode.springbootraideriolib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRaiderioLibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
